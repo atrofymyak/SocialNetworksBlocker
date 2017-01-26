382679d8c812ed937bcf05b69259d832f16b6ba8
Andriy Trofymyak <andriy_trofymyak@epam.com>
2017-01-26 17:43:01 +0200

    first commit

---
+++ src/com/Main.java
@@ -0,0 +1,70 @@
+package com;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+
+public class Main {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
+		for (;;) {
+			IOUtils.copy(new FileInputStream("c:\\Windows\\System32\\drivers\\etc\\hosts"),
+					new FileOutputStream("hosts"));
+
+			GregorianCalendar cal = new GregorianCalendar();
+			try {
+				cal = DateUtils.getAtomicTime();
+			} catch (Exception e) {
+
+			}
+
+			IOUtils.copy(new FileInputStream("hosts"), new FileOutputStream("hosts1"));
+			List<String> social = Files.readAllLines(Paths.get("social.txt"));
+			List<String> lines = Files.readAllLines(Paths.get("hosts1"));
+			System.out.println(cal.getTime());
+
+			if (cal.get(Calendar.HOUR_OF_DAY) >= 22) {
+
+				Files.write(Paths.get("hosts1"), "".getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+
+				for (String line : lines) {
+					if (!social.contains(line)) {
+						Files.write(Paths.get("hosts1"), line.getBytes(), StandardOpenOption.APPEND);
+						Files.write(Paths.get("hosts1"), "\r\n".getBytes(), StandardOpenOption.APPEND);
+					}
+				}
+
+				Files.write(Paths.get("hosts1"), Files.readAllBytes(Paths.get("social.txt")),
+						StandardOpenOption.APPEND);
+
+				Files.copy(Paths.get("hosts1"), new FileOutputStream("c:\\Windows\\System32\\drivers\\etc\\hosts"));
+			} else if (cal.get(Calendar.HOUR_OF_DAY) >= 7) {
+
+				Files.write(Paths.get("hosts1"), "".getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+
+				for (String line : lines) {
+					if (!social.contains(line)) {
+						Files.write(Paths.get("hosts1"), line.getBytes(), StandardOpenOption.APPEND);
+						Files.write(Paths.get("hosts1"), "\r\n".getBytes(), StandardOpenOption.APPEND);
+					}
+				}
+
+				Files.copy(Paths.get("hosts1"), new FileOutputStream("c:\\Windows\\System32\\drivers\\etc\\hosts"));
+			}
+
+			System.out.println("done");
+
+			Thread.sleep(60 * 1000 * 10);
+		}
+	}
+
+}
